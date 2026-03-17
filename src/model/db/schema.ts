ce5e2132f9c1a271b37982277100310934050ba9
donghch <dieshenke@gmail.com>
2026-03-16 21:25:54 -0700

    Basic database setup

---
+++ src/model/db/schema.ts
@@ -0,0 +1,8 @@
+import { sqliteTable, text, integer, real } from "drizzle-orm/sqlite-core";
+
+export const users = sqliteTable("users", 
+    {
+        id: integer("id").primaryKey(),
+        name: text("name").notNull(), 
+        email: text("email").notNull().unique()
+    });
